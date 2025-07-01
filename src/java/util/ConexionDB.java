f578ee2640e2ff8fbda4d12bbffad8bdba208bdb
Noome24 <noom.direct@gmail.com>
2025-06-30 21:33:12 -0500

    update

--- src/java/util/ConexionDB.java
+++ src/java/util/ConexionDB.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package util;
 
 import java.sql.Connection;
@@ -9,16 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionDB {
-    private static final String JDBC_URL = "jdbc:mysql://switchyard.proxy.rlwy.net:12070/railway?useSSL=false&serverTimezone=UTC";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/sistema_distribuido?useSSL=false&serverTimezone=UTC";
     private static final String JDBC_USER = "root";
-    private static final String JDBC_PASSWORD = "fyWKiPxAWQsAsMDztgpGYSFPtoVqthyv";
+    private static final String JDBC_PASSWORD = "administracion2411";
 
-
-    // Opcional: cargar driver MySQL en bloque estático si es necesario (para versiones antiguas de Java)
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
+            System.err.println("Error cargando el driver MySQL: " + e.getMessage());
         }
     }
 
